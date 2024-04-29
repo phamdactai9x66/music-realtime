bb1b936f198bb4cf35b957b968d574e8473d51a0
tai.pham <tai15122003311@gmail.com>
2024-04-30 00:42:41 +0700

    draw ui search songs

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import * as React from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import SearchIcon from "@mui/icons-material/Search";
+import ListSongs from "./Components/ListSongs";
 
 type HomeProps = object & React.PropsWithChildren;
 
 const Home: React.FC<HomeProps> = () => {
-  return <div>Home</div>;
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <FormControl fullWidth sx={{ m: 1 }}>
+        <InputLabel htmlFor="outlined-adornment-amount">
+          Search Songs
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-amount"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+          label="Search Songs"
+        />
+      </FormControl>
+
+      <ListSongs />
+    </div>
+  );
 };
 
 export default Home;
