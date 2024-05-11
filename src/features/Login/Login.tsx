bba89b63b5c4477ed33b279ddecfd67f3c8a7975
tai.pham <tai15122003311@gmail.com>
2024-05-11 23:41:57 +0700

    add login by facebook, google

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,11 +1,14 @@
+import * as React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import Stack from "@mui/material/Stack";
+
 import { Button, Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import React from "react";
 
 type LoginProps = object & React.PropsWithChildren;
 
 const useStyle = makeStyles((theme: Theme) => {
-  console.log(theme);
   return {
     containerBox: {},
   };
@@ -16,7 +19,14 @@ const Login: React.FC<LoginProps> = () => {
 
   return (
     <div className={classes.containerBox}>
-      <Button>Login</Button>
+      <Stack direction="row" spacing={2} justifyContent="space-evenly">
+        <Button variant="outlined" startIcon={<FacebookIcon />}>
+          Facebook
+        </Button>
+        <Button variant="outlined" startIcon={<GoogleIcon />}>
+          Google Gmail
+        </Button>
+      </Stack>
     </div>
   );
 };
