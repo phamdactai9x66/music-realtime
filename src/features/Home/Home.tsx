1ee89e8b5e0c60c0589053c2dad9c449937ae130
tai.pham <tai15122003311@gmail.com>
2024-05-13 00:32:41 +0700

    implement firebase database

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -6,10 +6,41 @@ import FormControl from "@mui/material/FormControl";
 import SearchIcon from "@mui/icons-material/Search";
 import ListSongs from "src/components/ui/ListItems";
 import SlideImage from "./Components/SlideImage";
+import { getDatabase, ref, set } from "firebase/database";
 
 type HomeProps = object & React.PropsWithChildren;
 
 const Home: React.FC<HomeProps> = () => {
+  const id = React.useId();
+  console.log(id);
+
+  const writeUserData = <T extends string>(
+    userId: T,
+    name: T,
+    email: T,
+    imageUrl: T
+  ) => {
+    const db = getDatabase();
+
+    set(ref(db, "users/" + userId), {
+      username: name,
+      email: email,
+      profile_picture: imageUrl,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  React.useEffect(() => {
+    writeUserData(id, "awdaw", "awdawd", "awdawd");
+
+    return () => {};
+  }, []);
+
   return (
     <div
       style={{
