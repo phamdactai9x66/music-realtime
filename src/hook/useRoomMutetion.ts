e7755c2b030fbd088027f4f9a54d5702709093b4
JJthang <thangpdph@gmail.com>
2024-05-26 11:19:06 +0700

    Update convention and structure file

--- src/hook/useRoomMutetion.ts
+++ src/hook/useRoomMutetion.ts
@@ -1,11 +1,11 @@
 import React from "react";
-import { RoomsUrl } from "src/apis/rooms.request";
+import { RoomsUrl } from "src/apis/request";
 import { roomsIf } from "src/models/Room.model";
 import httpRequest from "src/service/httpRequest";
 
 const useRoomMutetion = () => {
   const [roomsData, setRoomsData] = React.useState<roomsIf[] | []>([]);
-  
+
   const searchRooms = async (...query: unknown[]) => {
     const data = await httpRequest.getData<roomsIf>(RoomsUrl(), ...query);
     setRoomsData(data);
