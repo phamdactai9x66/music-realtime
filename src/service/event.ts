34f02b4aeb32454cc2cdef3e1d92c583f22bdb92
tai.pham <tai15122003311@gmail.com>
2024-06-06 00:22:09 +0700

    custom event

---
+++ src/service/event.ts
@@ -0,0 +1,20 @@
+type listenerCb = (this: Document, ev: any) => any;
+
+export enum LIST_EVENT {
+  CURRENT_SONG = "CURRENT_SONG",
+}
+
+function subscribe(eventName: LIST_EVENT, listener: listenerCb) {
+  document.addEventListener(eventName, listener);
+}
+
+function unsubscribe(eventName: LIST_EVENT, listener: listenerCb) {
+  document.removeEventListener(eventName, listener);
+}
+
+function publish(eventName: LIST_EVENT, data: looseObj) {
+  const event = new CustomEvent(eventName, { detail: data });
+  document.dispatchEvent(event);
+}
+
+export { publish, subscribe, unsubscribe };
